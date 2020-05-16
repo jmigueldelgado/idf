1f5d06bebb8b6aef2ff66ead93ce439959177920
JM Delgado <jmigueldelgado@gmail.com>
2020-05-16 23:57:14 +0200

    initial commit

---
+++ R/idf.R
@@ -0,0 +1,17 @@
+#' Obter intensidade num ponto no norte de portugal a partir da duração da chuvada e período de retorno
+#' @importFrom dplyr "%>%"
+#' @export
+df2i = function(d,f,lugar)
+{
+  lugar_sp=sf::as_Spatial(lugar)
+
+  idf_sp=idf_sf %>%
+    dplyr::filter(limite_superior>d & limite_inferior<d,T==f) %>%
+    dplyr::select(a,b) %>%
+    dplyr::mutate(i=a*as.numeric(d,'minutes')^b) %>%
+    sf::as_Spatial(.)
+
+  i_pred=gstat::idw(i~1,idf_sp,newdata=lugar_sp)
+  return(i_pred$var1.pred)
+
+}
---
+++ R/idf_sf-data.R
@@ -0,0 +1,19 @@
+#' Parâmetros das curvas IDF
+#'
+#' Parâmetros das curvas IDF obtidas por \href{https://snirh.apambiente.pt/snirh/download/relatorios/relatorio_prec_intensa.pdf}{Brandão e Rodrigues 2001}
+#'
+#' @docType data
+#'
+#' @usage data(idf)
+#'
+#' @format An object of class \code{"tibble"}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @source \href{http://www.civil.ist.utl.pt/~mps/Mod_hid/Teoria/teoria 2007 para jpg.pdf}{Apontamentos de modelação hidrológica da Professora Maria Manuela Portela}
+#'
+#' @examples
+#' data(idf)
+#' \donttest{head(idf)}
+"idf_sf"
