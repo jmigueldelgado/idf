2e366032270e26bacafcb911623a1dfd3a48d703
JM Delgado <jmigueldelgado@gmail.com>
2020-05-28 12:00:22 +0200

    improves readme and checks classes of parameters

--- R/idf.R
+++ R/idf.R
@@ -1,8 +1,19 @@
 #' Obter intensidade num ponto no norte de portugal a partir da duração da chuvada e período de retorno
-#' @importFrom dplyr "%>%"
+#' @param d é uma duração de classe _Duration_
+#' @param f é um período de retorno em anos, por exemplo 20
+#' @param lugar é um ponto no espaço de classe _sf_
+#' @importFrom dplyr "%>%" filter mutate select
+#' @importFrom gstat idw
+#' @importFrom sf as_Spatial st_crs st_transform
 #' @export
-df2i = function(d,f,lugar)
+intensidade = function(d,f,lugar)
 {
+  if(class(d)[1]!='Duration') stop('*d* must be of class Duration')
+  if(!(f %in% c(2,5,10,20,50,100,500,1000))) stop('*f* must be one of 2,5,10,20,50,100,500 or 1000')
+  if(class(lugar)[1]!='sf') stop('*lugar* must be of class sf')
+  
+  if(toupper( st_crs(lugar)[1]$input ) != 'EPSG:3763') lugar=st_transform(lugar,3763)
+
   lugar_sp=sf::as_Spatial(lugar)
 
   idf_sp=idf_sf %>%
---
+++ README.md
@@ -0,0 +1,24 @@
+# idf
+
+Este pacote pretende ajudar engenheiros e técnicos a estimar uma chuvada de projecto a partir das curvas de intensidade-duração-frequência propostas por [[1]](#1).
+
+## Instalação
+
+```{r}
+devtools::install_github('jmigueldelgado/idf')
+```
+
+## Como usar
+
+Chamar a função `intensidade` com os parâmetros duração `d` de classe _sf_, período de retorno `f` e um ponto georreferenciado `lugar` de classe _sf_.
+
+## Domínio de utilização
+
+Neste momento o pacote só está disponível para a região acima do Mondego.
+
+## Exemplo
+
+Para um exemplo remeto à [vignette](vignettes/idf_bacia_sousa.Rmd)
+
+## References
+<a id="1">[1]</a> Brandão, C.; Rodrigues, R.; Costa, J. P. Análise de Fenómenos Extremos. Precipitações Intensas Em Portugal Continental. Direcção dos Serviços de Recursos Hídricos 2001, 64.
